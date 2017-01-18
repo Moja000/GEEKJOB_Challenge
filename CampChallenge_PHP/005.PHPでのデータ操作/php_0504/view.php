d0fb62e2f97d2b1b015feb6236e62e30ad58724d
田中 元春 <motoharu.tanaka.0418@gmail.com>
2017-01-18 16:28:10 +0900

    PHP/データ操作04 提出します。 ご確認お願い致します。

---
+++ CampChallenge_PHP/005.PHPでのデータ操作/php_0504/view.php
@@ -0,0 +1,28 @@
+<HTML>
+  <HEAD>
+    <TITLE>PHPのテスト</TITLE>
+  </HEAD>
+  <BODY>
+    <?php
+        $file_dir = 'Applications/MAMP/htdoccs/php_05/php_0504/image/';
+        var_dump($_FILES);
+        $file_path = $file_dir.$_FILES["uploadfile"]["name"];
+
+        if(move_uploaded_file($_FILES["uploadfile"]["tmp_name"],$file_path)){
+
+            $img_dir = "/image/";
+            $img_path = $img_dir.$_FILES["uploadfile"]["name"];
+            $size = getimagesize($file_path);
+    ?>
+    ファイルアップロードを完了しました。<br>
+    <IMG src = "<?=$img_path?>"<?=$size[3]?>><br>
+    <B><?=$_POST["comment"]?></B><br>
+    <?php
+  }else{
+    ?>
+    正常にアップロードされませんでした。<br>
+    <?php
+  }
+  ?>
+  </BODY>
+  </HTML>
